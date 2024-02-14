042e2cc1520312dd2d707bd36afd63144f210c6e
juan bernal <mesieou@gmail.com>
2024-02-14 15:29:47 +1100

    added the location form and styled it

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get '/booking/locations/new', to: 'locations#new', as: 'new_location'
+  post '/booking/locations', to: 'locations#create', as: 'locations'
 
   resources :booking do
   end
