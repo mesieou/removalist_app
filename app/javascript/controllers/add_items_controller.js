00d5f84d52308b3929fba3a2e5c94463adfb5bd8
juan bernal <mesieou@gmail.com>
2024-02-25 19:04:53 +0800

    working on the add items to the cart feature

--- app/javascript/controllers/add_items_controller.js
+++ app/javascript/controllers/add_items_controller.js
@@ -2,13 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="add-items"
 export default class extends Controller {
-  connect() {
-    console.log("Hello, Stimulus!")
-  }
+  static targets = [ "items" ]
+
   // Connects to data-action="click->add-items#add"
-  add(event) {
+  select(event) {
     event.currentTarget.classList.toggle('minimalist-button-active')
     console.log(event.currentTarget)
-
+  }
+  // Connects to data-action="click->add-items#add"
+  add() {
+    const itemsSelected = this.itemsTarget.querySelectorAll('.minimalist-button-active')
+    const itemsSelectedArray = Array.from(itemsSelected)
+    itemsSelectedArray.forEach((item) => {
+      debugger;
+      console.log(item.textContent)
+      // find the item in the database
+      // create the cart if it doesn't exist
+      // add the item to the cart
+      // show the cart
+    })
   }
 }
