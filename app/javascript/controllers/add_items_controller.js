b5bb22b8e57ea297fb351f0faef623365bb1dc4e
juan bernal <mesieou@gmail.com>
2024-02-26 00:29:44 +0800

    modal added

--- app/javascript/controllers/add_items_controller.js
+++ app/javascript/controllers/add_items_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="add-items"
 export default class extends Controller {
-  static targets = [ "items" ]
+  static targets = [ "items", "modal" ]
   static values = { time: Number }
 
   // Connects to data-action="click->add-items#add"
@@ -14,14 +14,14 @@ export default class extends Controller {
   add() {
     const itemsSelected = this.itemsTarget.querySelectorAll('.minimalist-button-active')
     const itemsSelectedArray = Array.from(itemsSelected)
+    const modal = this.modalTarget
     itemsSelectedArray.forEach((item) => {
       const itemText = item.textContent
       const itemTime = Number(item.dataset.addItemsValue)
-      debugger;
-      // find the item in the database
-      // create the cart if it doesn't exist
-      // add the item to the cart
-      // show the cart
+      const itemHTML = `<div class="cart-item">${itemText}</div>`
+      modal.insertAdjacentHTML('beforeend', itemHTML)
+      item.classList.remove('minimalist-button-active')
     })
+
   }
 }
