f58a4acd8c87bd364abb2f80d6496f94762c0b71
juan bernal <mesieou@gmail.com>
2024-03-08 16:27:20 +1100

    direction service google api added to calculate distance

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -17,11 +17,29 @@ export default class extends Controller {
     loader
       .load()
       .then((google) => {
-        this.pickupAutocomplete = new google.maps.places.Autocomplete(this.pick_upTarget);
-        this.dropoffAutocomplete = new google.maps.places.Autocomplete(this.drop_offTarget);
+        // this.pickupAutocomplete = new google.maps.places.Autocomplete(this.pick_upTarget);
+        // this.dropoffAutocomplete = new google.maps.places.Autocomplete(this.drop_offTarget);
       })
       .catch((e) => {
         console.log("error loading gmaps")
       })
+  }
+
+  distance () {
+    const pickUp = this.pick_upTarget.value;
+    const dropOff = this.drop_offTarget.value;
+    var directionsService = new google.maps.DirectionsService();
+    let request = {
+      origin: pickUp,
+      destination: dropOff,
+      travelMode: 'DRIVING'
+    }
+
+    directionsService.route(request, function(result, status){
+      if (status == 'OK') {
+        console.log(result.routes[0].legs[0].distance.text)
+        console.log(result.routes[0].legs[0].duration.text)
+      }
+    });
   }
 }
